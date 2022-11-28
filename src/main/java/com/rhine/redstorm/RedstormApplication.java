0673d04abef9a3ca8a823009e6addfc26f78c9cb
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-28 02:08:48 +0100

    Initial commit

---
+++ src/main/java/com/rhine/redstorm/RedstormApplication.java
@@ -0,0 +1,13 @@
+package com.rhine.redstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedstormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedstormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhine/redstorm/RedstormApplicationTests.java
@@ -0,0 +1,20 @@
+package com.rhine.redstorm;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedstormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void nTests() {
+		assertTrue(true);
+	}
+
+}
