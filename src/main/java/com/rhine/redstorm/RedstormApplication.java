870a91fe402d34ae1a9017d2041e8bcce4c6f287
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-12-05 02:20:16 +0100

    Add component scanning

--- src/main/java/com/rhine/redstorm/RedstormApplication.java
+++ src/main/java/com/rhine/redstorm/RedstormApplication.java
@@ -2,8 +2,10 @@ package com.rhine.redstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.rhine.redstorm.*")
 public class RedstormApplication {
 
 	public static void main(String[] args) {
