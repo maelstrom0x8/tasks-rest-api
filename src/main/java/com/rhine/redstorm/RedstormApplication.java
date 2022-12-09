28183b042e8583e918999c85cc08eed6c18d97fe
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-12-09 19:47:07 +0100

    Add component scanning to discover beans in subpackages

--- src/main/java/com/rhine/redstorm/RedstormApplication.java
+++ src/main/java/com/rhine/redstorm/RedstormApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.rhine.redstorm.*")
+@ComponentScan
 public class RedstormApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/rhine/redstorm/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.rhine.redstorm.config;
+
+// @EnableWebSecurity
+public class SecurityConfig {
+
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     return http
+    //             .authorizeHttpRequests(auth -> {
+    //                 auth.anyRequest().permitAll();
+    //             })
+    //             .httpBasic(Customizer.withDefaults())
+    //             .build();
+    // }
+
+}
