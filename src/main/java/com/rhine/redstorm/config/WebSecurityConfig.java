c21a2c7baeb1313360729113f4f7258bce4ab13d
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-16 03:02:09 +0100

    add : base security configs

---
+++ src/main/java/com/rhine/redstorm/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.rhine.redstorm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeHttpRequests(auth -> {
+            auth.anyRequest().authenticated();
+
+        }).oauth2ResourceServer(oauth -> oauth.jwt());
+
+        return http.build();
+    }
+
+}
