17678bc55e4940eb631e5b7c6117900a522b8cec
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-25 07:38:06 +0100

    resource server configs

--- src/main/java/com/rhine/redstorm/config/RedstormConfig.java
+++ src/main/java/com/rhine/redstorm/config/RedstormConfig.java
@@ -1,15 +1,21 @@
-package com.rhine.redstorm;
+package com.rhine.redstorm.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RedstromConfig {
+public class RedstormConfig {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
